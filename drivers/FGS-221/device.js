70ea4d5119787b3b66be25e00872113377fac909
Jan <jan.pekkeriet@gmail.com>
2019-11-29 14:01:14 +0100

    Update device.js

--- drivers/FGS-221/device.js
+++ drivers/FGS-221/device.js
@@ -6,6 +6,8 @@ class FibaroDoubleSwitchDevice extends ZwaveDevice {
 
 	onMeshInit() {
 		this.registerCapability('onoff', 'SWITCH_BINARY');
+		this._input1FlowTrigger = this.getDriver().input1FlowTrigger;
+		this._input2FlowTrigger = this.getDriver().input2FlowTrigger;
 	}
 
 }
